0ae9043a81fb30885c1df536d946b013b6241778
alinaltyn <alieneguzh@gmail.com>
2024-12-06 18:59:41 +0100

    add exercise 1, level1

---
+++ S04T01N02/src/main/java/cat/itacademy/s04/t01/n02/HelloWorldController.java
@@ -0,0 +1,25 @@
+package cat.itacademy.s04.t01.n02;
+
+import ch.qos.logback.core.util.StringUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public String hello(@RequestParam(value = "name", defaultValue = "UNKNOWN")String name){
+        return "Hello, " + name + ". You are running a Gradle project.";
+    }
+
+    @GetMapping(value = {"/HelloWorld2","/HelloWorld2/{name}"})
+    private String hello2(@PathVariable(required = false) String name){
+        if(!StringUtil.isNullOrEmpty(name)){
+            return "Hello, " + name + ". You are running a Gradle project.";
+        } else {
+            return "Hello, UNKNOWN! You are running a Gradle project.";
+        }
+    }
+}
